aa72e1c3c23dcf26079b3ff40c24fd68dda8917e
Rahul <31180528+guntharahul@users.noreply.github.com>
2021-12-08 22:37:52 -0700

    Intial Project Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  
+  return <div className="App">Hello</div>;
+}
+
+export default App;
