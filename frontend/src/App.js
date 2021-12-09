f4ed6e41a0e4b0640e855b372d62a4e579e7ac13
Rahul <31180528+guntharahul@users.noreply.github.com>
2021-12-09 11:26:41 -0700

    Designing Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./components/screens/HomeScreen";
 import ProductScreen from "./components/screens/ProductScreen";
+
 // BrowserRouter is similar to that of HTML5 history API which uses push state and replace state
 function App() {
   return (
@@ -12,13 +13,10 @@ function App() {
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen></HomeScreen>} exact></Route>
-            <Route
-              path="/product/:id"
-              element={<ProductScreen></ProductScreen>}
-            ></Route>
-          </Routes>
+          <Switch>
+            <Route path="/" component={HomeScreen} exact></Route>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+          </Switch>
         </Container>
       </main>
       <Footer></Footer>
