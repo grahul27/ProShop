0f6513ba5995310b586b73120f39748fbfc1a74c
Rahul <31180528+guntharahul@users.noreply.github.com>
2021-12-13 01:13:53 -0700

    Changing Backend server into ES6 module enability

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
-const express = require("express");
-const app = express();
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
-app.get("/", (req, res) => {
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res, next) => {
   res.send("api is running");
 });
 
-app.get("/api/products", (req, res) => {
+app.get("/api/products", (req, res, next) => {
   res.json(products);
 });
 
-app.get("/api/products/:id", (req, res) => {
+app.get("/api/products/:id", (req, res, next) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log(`Server running on port 5000`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
 });
