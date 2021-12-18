c964e42d0ae4107688ff4586292d7a78aded8031
Rahul <31180528+guntharahul@users.noreply.github.com>
2021-12-18 11:58:18 -0700

    creating routes and async-handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 //Database connection
@@ -14,14 +14,7 @@ app.get("/", (req, res, next) => {
   res.send("api is running");
 });
 
-app.get("/api/products", (req, res, next) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res, next) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(
